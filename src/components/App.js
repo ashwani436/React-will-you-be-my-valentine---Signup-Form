804842ec7200d5af82d1481eb9d166b16ce2b24a
ashwani436 <sharmagpl786@gmail.com>
2021-02-20 23:11:46 +0530

    save changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,37 +1,78 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import MainForm from "./MainForm/";
+//import "./styles/App.css";
 
 const App = () => {
- const onChangeAlphaNumericInput=(e)=>{
-    const value = e.target.value;
-    const regex = /^[0-9a-zA-Z(\-)]+$/;//this will admit letters, numbers and dashes
-    if (value.match(regex)||value ===""){
-      this.setState({inputValue:value});
+  const [uName, setUname] = useState("");
+  const [eMail,setEmail] = useState("");
+  const [gender,setGender] = useState("Male");
+  const [phone,setPhone] = useState("");
+  const [password,setPassword] = useState("");
+  const [errorMsg,setErrormsg] = useState("");
+
+  const handleName = (event) => {
+    setUname(event.target.value);
+  };
+
+  const handleEmail = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handleGender = (event) => {
+    setGender(event.target.value);
+  };
+
+  const handlePhone = (event) => {
+    setPhone(event.target.value);
+  };
+
+  const handlePassword = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (e) =>{
+    console.log("submit button clicked");
+    // console.log(uName);
+    // console.log(eMail);
+    // console.log(gender);
+    // console.log(phone);
+    // console.log(password);
+    if(!uName || !eMail || !phone || !password ){
+      setErrormsg("All fields are mandatory");
+      e.preventDefault();
+      return;
     }
-  }
-  const submit=()=>{
-        
+    else if(!uName.match(/^[0-9a-z]+$/)){
+      setErrormsg("Name is not alphanumeric");
+      e.preventDefault();
+      return;
+    }
+    else if(!eMail.match(/\S+@\S+\.\S+/)){
+      setErrormsg("Email must contain @");
+      e.preventDefault();
+      return;
+    }
+    else{
+      var name = eMail.substring(0, eMail.lastIndexOf("@"));
+      setErrormsg("Hello "+name);
+      e.preventDefault();
+    }
+    console.log("submitted")
   }
   return (
     <div id="main">
-        <label>Name:</label>
-        <input type="text" data-testid ='name' onClick={()=>onChangeAlphaNumericInput(e)}/><br/>
-        <label> Email address:</label>
-        <input type="email" data-testid = 'email'/><br/>
-        <label>Gender: </label>
-        <select>
-                  <option name="male">male</option>
-                  <option name="female">female</option>
-                  <option name="other">other</option>
-              </select><br/>
-        <label>Phone Number:</label>
-        <input type="number" data-testid = 'phoneNumber'/><br/>
-        <label>Password:</label>
-        <input data-testid = 'password' type='password'/><br/>
-        <button type="submit">Submit</button>
+      <MainForm
+        handleName={handleName}
+        handleEmail={handleEmail}
+        handleGender={handleGender}
+        defGender={gender}
+        handlePhone={handlePhone}
+        handlePassword={handlePassword}
+        handleSubmit={handleSubmit}
+        showError={errorMsg}
+      />
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
